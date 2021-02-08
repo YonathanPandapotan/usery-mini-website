d0a60fe11cf5e5e2db0b989f7f7855cb7c47b08c
yona than <yonabaho@gmail.com>
2021-02-08 14:44:46 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>UserY Mini Website</title>
+        <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Yonathan Pandapotan</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/img/profile/profile.jpg" alt="profile" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Tentang Saya</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Pengalaman</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Pendidikan</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Kemampuan</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Sertifikasi</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Yonathan
+                        <span class="text-primary">Pandapotan</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        0812 9070 9542 ·
+                        <a href="mailto:yonabaho@gmail.com">yonabaho@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Perkenalkan saya adalah Yonathan Pandapotan. Selamat datang pada website mini saya.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/yonathan-pandapotan-3663911bb/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/YonathanPandapotan"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://web.facebook.com/yonathanpandapotan/"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Pengalaman</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Membangun Website Sekolah</h3>
+                            <div class="subheading mb-3">Bagian backend</div>
+                            <p>Saya sebelumnya sudah pernah membuat website sekolah menggunakan framework laravel. Website sekolah ini berisi tentang data dan informasi mengenai sekolah seperti visi, misi, tujuan, dsb. Selain itu website ini juga memiliki sistem penyimpanan data untuk menyimpan data mahasiswa dan guru. Sehingga pengunjung dapat melihat tenaga pengajar yang terdapat di sekolah. Kemudian ada sistem untuk membuat berita acara beserta dengan galeri untuk menunjukkan aktifitas yang ada di sekolah tersebut.</p>
+                            <h4 class="mb-0">Screenshot</h4>
+                            <div class="pop-container">
+                                <img class="pop img-thumbnail" src="/img/project/sekolah-home.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/sekolah-berita.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/sekolah-admin-dashboard.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/sekolah-admin-berita-form.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/sekolah-admin-berita-panel.png" style="width: 400px; height: 264px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Simple Bootstrap Web</h3>
+                            <div class="subheading mb-3">Projek Tugas Kuliah</div>
+                            <p>Website yang ini merupakan website untuk tugas kuliah dimana tugas ini diperlukan menggunakan bootstrap untuk mengerjakannya. Konten nya sendiri bersifat statis atau tidak memiliki backend.</p>
+                            <div class="pop-container">
+                                <img class="pop img-thumbnail" src="/img/project/simple-bootstrap-web.herokuapp.com_about.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/simple-bootstrap-web.herokuapp.com_detailpost.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/simple-bootstrap-web.herokuapp.com_index.png" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/simple-bootstrap-web.herokuapp.com_post.png" style="width: 400px; height: 264px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Simple Contact Apps</h3>
+                            <div class="subheading mb-3">Mencoba membangun aplikasi kontak simple</div>
+                            <p>Aplikasi ini dibuat di platform Android dengan menggunakan Android Studio. Tujuan dari aplikasi ini untuk mencoba membuat aplikasi kontak simple. Aplikasi ini menggunakan teknologi SQLite untuk melakukan penyimpanan data.</p>
+                            <div class="pop-container">
+                                <img class="pop img-thumbnail" src="/img/project/Screenshot_2021-01-12-20-30-15-887_com.local.asus.simplecontactapps.jpg" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/Screenshot_2021-01-12-20-30-21-145_com.local.asus.simplecontactapps.jpg" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/Screenshot_2021-01-12-20-30-25-234_com.local.asus.simplecontactapps.jpg" style="width: 400px; height: 264px;">
+                                <img class="pop img-thumbnail" src="/img/project/Screenshot_2021-01-12-20-34-29-266_com.local.asus.simplecontactapps.jpg" style="width: 400px; height: 264px;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Pendidikan Formal</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Universitas Esa Unggul</h3>
+                            <div class="subheading mb-3">Sarjana Komputer (S.Kom)</div>
+                            <div>Fakultas Ilmu Komputer - Jurusan Teknik Informatika</div>
+                            <p>IPK: 3.61</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2016 - 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMA Agape BKKK</h3>
+                            <div class="subheading mb-3">Jurusan IPA</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2013 - 2016</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Kemampuan</h2>
+                    <div class="subheading mb-3">Bahasa Pemrograman</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-circle"></i></span>
+                            Android (Kotlin, Java)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-circle"></i></span>
+                            Web (Html, javascript, jquery, laravel)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-circle"></i></span>
+                            Database (SQL, SQLite)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-circle"></i></span>
+                            Python (sklearn, matplotlib, numpy)
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Sertifikasi</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/L4PQM62MVZO1">Belajar Membuat Aplikasi Kognitif</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/QLZ98261DX5D">Belajar Pengembangan Machine Learning</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/2VX3NY52JXYQ">Belajar Machine Learning untuk Pemula</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/MRZMG46KNZYQ">Belajar Dasar Visualisasi Data</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/07Z6LGJ5MPQR">Memulai Pemrograman Dengan Python</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/98XW64OK9XM3">Belajar Membangun LINE Chatbot</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/KEXL492G4XG2">Belajar Membuat Aplikasi Android untuk Pemula</a>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-certificate"></i></span>
+                            Dicoding - <a href="http://dicoding.com/certificates/QLZ9826KDX5D">Memulai Pemrograman Dengan Kotlin</a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">              
+                        <div class="modal-body">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                        <img src="" class="imagepreview" style="width: 100%;" >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="/js/scripts.js"></script>
+    </body>
+</html>
